941a19dfa0efb9914af24ae17a4108a4c49ecbeb
Shahriar Fardows <shahriarfardows@gmail.com>
2023-10-04 04:18:58 +0600

    first commit

---
+++ js/price.js
@@ -0,0 +1,54 @@
+document.getElementById('K.Accessories').addEventListener('click', function() { 
+     items('K. Accessories');
+    const price = priceAdd(39);
+    return price ;
+  
+});
+document.getElementById('K.Accessories2').addEventListener('click', function() { 
+    items('K. Accessories2');
+    const price = priceAdd(25);
+    return price;
+
+});
+document.getElementById('HomeCooker').addEventListener('click', function() { 
+    items('Home Cooker');
+    const price = priceAdd(49);
+    return price;
+  
+});
+document.getElementById('Sports-Back-Cap').addEventListener('click', function() { 
+    items('Sports Back Cap');
+    const price = priceAdd(49);
+    return price;
+  
+});
+document.getElementById('Full-Jersey-Set').addEventListener('click', function() { 
+    items('Full Jersey-Set');
+    const price = priceAdd(69);
+    return price;
+  
+});
+document.getElementById('Sports-cates').addEventListener('click', function() { 
+    items('Sports cates');
+    const price = priceAdd(159);
+    return price;
+  
+});
+document.getElementById('Single-Relax-Chair').addEventListener('click', function() { 
+    items('Single Relax Chair');
+    const price = priceAdd(185);
+    return price;
+  
+});
+document.getElementById('Children-play').addEventListener('click', function() { 
+    items('Children play');
+    const price = priceAdd(299);
+    return price;
+  
+});
+document.getElementById('Flexible-Sofa').addEventListener('click', function() { 
+    items('Flexible Sofa');
+    const price = priceAdd(399);
+    return price;
+  
+});
---
+++ js/same.js
@@ -0,0 +1,75 @@
+// aita holo card theke product er price total price e add korar jonno function likhsi
+function priceAdd(price) {
+    const priceAdd = document.getElementById('Total-price');
+    const prices = parseFloat(priceAdd.innerText);
+    const priceNow =price;
+    priceAdd.innerText = prices + priceNow;
+    totalPrice = priceAdd.innerText;
+    checkActivation();
+    return priceAdd.innerText;
+    
+    
+}
+
+
+// aita holo card er name  iteam name e add korar jonno function likhsi
+function items(name){
+    const addItem = document.getElementById('items-name');
+    const count = addItem.childElementCount;
+    const h4 = document.createElement('h4');
+    h4.classList.add('my-6');
+    h4.innerHTML= `${count}. ${name}`;
+    addItem.appendChild(h4);
+}
+
+function addTen(price) {
+    totalPrice = price;
+    totalPriceTag.innerText = totalPrice;
+    checkActivation();
+}
+
+// agula holo button er id gula
+
+const coupon = document.getElementById('coupon');
+const activateButton = document.getElementById("Apply");
+const payBtn = document.getElementById("pay");
+
+var totalPrice = 0;
+
+
+// coupon code apply korar jonno function likha hosse 
+
+document.getElementById('Apply').addEventListener('click', function() {
+    const couponCode = coupon.value;
+    if (couponCode == "SELL20") {
+      const discount = document.getElementById('Discount');
+        const discountPrice = parseFloat(discount.innerText);
+        const discountNow = totalPrice * 0.2;
+        discount.innerText = discountNow;   
+        const totalPriceTagS = document.getElementById('Total-price-now');
+        const totalPriceTag = parseFloat(totalPriceTagS.innerText);
+        const priceNow = totalPrice - discountNow;
+        totalPriceTagS.innerText = priceNow;
+
+
+    }
+    else {
+        alert("Invalid Cupon Code");
+    }
+    coupon.value = "";
+});
+
+// ai ta apply button active er jonno function likhsi
+
+function checkActivation() {
+    if (totalPrice >= 200) {
+        activateButton.removeAttribute("disabled");
+    }
+    else if(totalPrice >=1) {
+        payBtn.removeAttribute("disabled");
+    }
+    else {
+        activateButton.setAttribute("disabled", true);
+        payBtn.setAttribute("disabled", true);
+    }
+}
